52283d6c35d56988eac58dd8e949738854b5f6c6
axel david <axel.floresb@hotmail.com>
2024-06-16 01:19:42 -0600

    commit raro

--- tarea-react/src/App.tsx
+++ tarea-react/src/App.tsx
@@ -1,13 +1,23 @@
-import Header from './components/Header'
-import Form from './components/form'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/login'
+import Register from './components/register'
+import MainPage from './components/mainPage';
+import UserPage from './components/userPage';
+import { UserProvider } from './contexts/UserContext';
 function App() {
   return (
-    <>
-      <Header title='Mi primer react'/>
-      <Form />
-    </>
+
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/userPage" element={<UserPage />} />
+        </Routes>
+      </Router>
+   </UserProvider>
+
   )
 }
-
 export default App
--- tarea-react/src/components/form.tsx
+++ tarea-react/src/components/form.tsx
@@ -1,30 +1,101 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap'
 import { Form } from 'react-bootstrap'
-import Data from './Data'
 import '../styles/form.css'
-const data = {
-    email: 'axel.floresb@hotmail.com',
-    password: '1234'
+const API_URL = "http://localhost:3010/";
+interface Animes {
+    name: string,
+    description: string,
+    genere: string,
+    score: number
+    user: object
 }
+/*
+    usuarios registrados:
+    
+    axel@hotmail.com
+    password: 1234
 
+    p@hotmail.com
+    password: 1234
+*/
 function form() {
     const [email, setEmail] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
-    const [showdata, setshowdata] = useState<boolean>(false)
+    const [password, setPassword] = useState<string>("")//
+    const [sesion, setSesion] = useState<boolean>(false)//bandera para la sesion
+    const [animes, setAnimes] = useState<Animes[]>([])//establecer animes del usuario
+
+    useEffect(() => {
+        const userid = localStorage.getItem("userid")
+        const token = localStorage.getItem("token")
+        if (userid && token) {
+            setSesion(true)
+            fetchAnime(userid,token)
+        }
+    }, [])
+
+    const logIn = async ({ email, password }: { email: string, password: string }) => {
+        try {
+            const response = await fetch(`${API_URL}api/v1/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),//convertir obj a JSON
+            });
+            if (!response.ok) {
+                alert("Usuario no encontrado")
+            }
+
+            const data = await response.json()
+            const userid = data.user.id
+            const userStorageID = window.localStorage.getItem("userid")
+
+            if (userStorageID === userid) {
+                alert("Usuario ya logeado")
+            } else {
+                localStorage.setItem("userid", userid)
+                localStorage.setItem("token", data.token)
+                setSesion(true)
+                fetchAnime(userid,data.token)
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const fetchAnime = async (id: string,token:string) => {
+        try {
+            const respose = await fetch(`${API_URL}api/v1/animes?userId=${id}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            if (!respose.ok) {
+                alert("Error al obtener los datos")
+            }
+            const data = await respose.json();
+            setAnimes(data)
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     const handleInputChange = (stateUpdate: any) => {
         return (e: any) => {
             stateUpdate(e.target.value)
         }
     }
+
     const handleOnClick = (e: any) => {
-        if (data.email === email && data.password === password) {
-            setshowdata(true)
+        if (!email || !password) {
+            alert("Ingrese contrasenia y correo por favor")
         } else {
-            alert("Datos incorrectos")
+            logIn({ email, password })
         }
+        //fetchAnime()
         e.preventDefault()
     }
     return (
@@ -48,7 +119,23 @@ function form() {
                     </Button>
                 </Form>
             </div>
-                <Data email={email} password={password} showdata={showdata} />
+            {
+                sesion && (
+                    <div className="animes">
+                        <h2>Animes listados</h2>
+                        {
+                            animes.map((anime, index) => (
+                                <div key={index} className="anime">
+                                    <h3>{anime.name}</h3>
+                                    <p>Description: {anime.description}</p>
+                                    <p>Genere: {anime.genere}</p>
+                                    <p>Score: {anime.score}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
+                )
+            }
         </div>
     )
 }
